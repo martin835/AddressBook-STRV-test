a644d5e3beaae3ee336062f466c1fb75ca886472
martin835 <64438132+martin835@users.noreply.github.com>
2022-09-04 18:43:10 +0200

    "commenting out email tools - not needed now"

--- src/tools/email-tools.js
+++ src/tools/email-tools.js
@@ -1,29 +1,26 @@
-import sgMail from "@sendgrid/mail";
+//JUST KEEPING THIS HERE FOR FUTURE "CONFIRM REGISTRATION BY EMAIL" FEATURE
+//--> need  to install and configure sendgrind package.
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// import sgMail from "@sendgrid/mail";
 
-export const sendRegistrationEmail = async (body) => {
-  //console.log("recieved body:", body);
-  const msg = {
-    //normally recipient email will come from request.author.email;
-    // ⬇️ ⬇️ ⬇️ this is just for the testing purposes
-    to: `${body.email}`,
-    from: process.env.SENDER_EMAIL,
-    subject: "Potvrďte Váš email",
-    text: body.content,
-    html: `<h3>Dobrý deň ${body.name},</h3>
-    <p>dokončite registráciu kliknutím na nasledujúci odkaz: </p>
-     <strong><a href=${body.link}>Overiť email</a></strong> 
-     <p>Pekný deň praje,</p>
-     <p>tím Odkaz štátu</p>
-     <div>------------------<strong>English</strong>------------------</div>
-     <h3>Hello ${body.name},</h3>
-    <p>finish the registration by clicking the activation link: </p>
-     <strong><a href=${body.link}>Overiť email</a></strong> 
-     <p>Have a great day,</p>
-     <p>team Tell the Government</p>
-    `,
-  };
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  await sgMail.send(msg);
-};
+// export const sendRegistrationEmail = async (body) => {
+//   //console.log("recieved body:", body);
+//   const msg = {
+//     //normally recipient email will come from request.author.email;
+//     // ⬇️ ⬇️ ⬇️ this is just for the testing purposes
+//     to: `${body.email}`,
+//     from: process.env.SENDER_EMAIL,
+//     subject: "Potvrďte Váš email",
+//     text: body.content,
+//     html: `<h3>Hello ${body.name},</h3>
+//     <p>finish the registration by clicking the activation link: </p>
+//      <strong><a href=${body.link}>Overiť email</a></strong>
+//      <p>Have a great day,</p>
+//      <p>team -- </p>
+//     `,
+//   };
+
+//   await sgMail.send(msg);
+// };
